f9829cca6fa15f89da0b6a0de111e26e29536c2a
Daniel Torres <danstorre@gmail.com>
2020-04-21 18:37:19 -0300

    add a session

---
+++ OMDb/Model/Session.swift
@@ -0,0 +1,13 @@
+//
+//  Session.swift
+//  OMDb
+//
+//  Created by Daniel Torres on 4/21/20.
+//  Copyright © 2020 dansTeam. All rights reserved.
+//
+
+import UIKit
+
+class Session: NSObject {
+
+}
