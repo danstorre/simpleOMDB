ac3550fe9b782e86aeefd519701851666b54f788
Daniel Torres <danstorre@gmail.com>
2020-04-21 19:29:00 -0300

    Add ui for Profile screen

--- OMDb/ViewController/ProfileViewController.swift
+++ OMDb/ViewController/ProfileViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet var userNameLabel: UILabel!
+    var session: SessionProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonPressed(_ sender: Any) {
     }
-    */
+    
+}
 
+extension ProfileViewController: PropertyObserver {
+    func didChange(propertyName: String, oldPropertyValue: Any?) {
+    }
+    
+    func willChange(propertyName: String, newPropertyValue: Any?) {
+        if propertyName == Session.SessionKeys.userKey, let user = newPropertyValue as? User {
+            userNameLabel.text = "Your name is \(user.name)"
+            print("userNameLabel as changed to \(user.name)")
+        }
+    }
 }
