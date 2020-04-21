3473a5f22506e5c6cfdc3862149a14e4f2874510
Daniel Torres <danstorre@gmail.com>
2020-04-21 19:52:15 -0300

    fill user profile info from session when user has log in

--- OMDb/Model/Session.swift
+++ OMDb/Model/Session.swift
@@ -26,11 +26,11 @@ final class Session: NSObject, SessionProtocol{
     }
     
     weak var observer:PropertyObserver?
-
+    
     enum SessionKeys {
         static let userKey = "user"
     }
-
+    
     var user: User {
         willSet(newValue) {
             observer?.willChange(propertyName: SessionKeys.userKey, newPropertyValue: newValue)
@@ -43,32 +43,39 @@ final class Session: NSObject, SessionProtocol{
 
 extension Session: GIDSignInDelegate{
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-               GIDSignIn.sharedInstance().clientID = "215368444628-j924tqlejb6b6a0bl6u3iu47dbegjo2d.apps.googleusercontent.com"
-               GIDSignIn.sharedInstance().delegate = self
-               
-               if let error = error {
-                 if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
-                   print("The user has not signed in before or they have since signed out.")
-                 } else {
-                   print("\(error.localizedDescription)")
-                 }
-                 return
-               }
-               // Perform any operations on signed in user here.
-       //        let userId = user.userID                  // For client-side use only!
-       //        let idToken = user.authentication.idToken // Safe to send to the server
-       //        let fullName = user.profile.name
-       //        let givenName = user.profile.givenName
-       //        let familyName = user.profile.familyName
-       //        let email = user.profile.email
-           }
-           
-           func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
-                     withError error: Error!) {
-             // Perform any operations when the user disconnects from app here.
-             // ...
-           }
-
+        GIDSignIn.sharedInstance().clientID = "215368444628-j924tqlejb6b6a0bl6u3iu47dbegjo2d.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().delegate = self
+        
+        if let error = error {
+            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out.")
+            } else {
+                print("\(error.localizedDescription)")
+            }
+            return
+        }
+        // Perform any operations on signed in user here.
+        let userId = user.userID                  // For client-side use only!
+        //        let idToken = user.authentication.idToken // Safe to send to the server
+        //        let fullName = user.profile.name
+        //        let familyName = user.profile.familyName
+        guard let givenName = user.profile.givenName,
+            let email = user.profile.email else{
+                print("couldn't retrieve user info")
+                self.user = UserNotLogged()
+                return
+        }
+        
+        let profileFromUserLoggedIn = Profile(id: userId ?? "1", email: email, name: givenName)
+        self.user = profileFromUserLoggedIn
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+        self.user = UserNotLogged()
+    }
 }
 
 protocol ObserverCollection {
@@ -89,7 +96,7 @@ final class SessionObserverMediator: ObserverMediator {
     func willChange(propertyName: String, newPropertyValue: Any?) {
         for sessionObserver in sessionObservers {
             sessionObserver.willChange(propertyName: propertyName,
-                                        newPropertyValue: newPropertyValue)
+                                       newPropertyValue: newPropertyValue)
         }
     }
     
