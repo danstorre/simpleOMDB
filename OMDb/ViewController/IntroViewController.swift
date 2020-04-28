5cc750aca4a1ae23a287053a8cb300083c9f0b85
Daniel Torres <danstorre@gmail.com>
2020-04-27 23:32:25 -0300

    finish textbox features in screen.

--- OMDb/ViewController/IntroViewController.swift
+++ OMDb/ViewController/IntroViewController.swift
@@ -12,6 +12,7 @@ class IntroViewController: UIViewController {
 
     @IBOutlet var headLabel: UILabel!
     @IBOutlet var bodyLabel: UILabel!
+    @IBOutlet var stackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,29 @@ class IntroViewController: UIViewController {
         headLabel.numberOfLines = 0
         headLabel.lineBreakMode = .byWordWrapping
         headLabel.backgroundColor = .clear
+        
+        bodyLabel.attributedText = TextFactory.attributedText(for: .body(string: bodyLabel.text!))
+        bodyLabel.numberOfLines = 0
+        bodyLabel.lineBreakMode = .byWordWrapping
+        bodyLabel.backgroundColor = .clear
+        
+        stackView.addArrangedSubview(TextFactoryTextBox.textBox(for: .featureTextBox(title: "Search Media",
+                                                                             titleColor: UIColor(named: "Blue1")!,
+                                                                             description: "From movies, series as well as episodes.",
+                                                                             image: UIImage(systemName: "doc.text.magnifyingglass")!,
+                                                                             iconColor: UIColor(named: "Blue1")!)))
+        stackView.addArrangedSubview(TextFactoryTextBox.textBox(for: .featureTextBox(title: "Take them with you",
+                                                                            titleColor: UIColor(named: "Blue1")!,
+                                                                            description: "Login with google and save your favorite media in the cloud.",
+                                                                            image: UIImage(systemName: "folder")!,
+                                                                            iconColor: UIColor(named: "Yellow")!)))
+        stackView.addArrangedSubview(TextFactoryTextBox.textBox(for: .featureTextBox(title: "Get notified",
+                                                                            titleColor: UIColor(named: "Blue1")!,
+                                                                            description: "Check out new products for your needs!",
+                                                                            image: UIImage(systemName: "bubble.left.and.bubble.right")!,
+                                                                            iconColor: UIColor(named: "Text")!)))
+        stackView.setNeedsLayout()
+        stackView.layoutIfNeeded()
         // Do any additional setup after loading the view.
     }
     
