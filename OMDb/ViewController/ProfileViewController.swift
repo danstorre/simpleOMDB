269588c70bc333e57d9ab2a6880c2554a2ec2caf
Daniel Torres <danstorre@gmail.com>
2020-05-04 15:08:30 -0300

    Give room to add user not logged and user logged views.

--- OMDb/ViewController/ProfileViewController.swift
+++ OMDb/ViewController/ProfileViewController.swift
@@ -11,20 +11,16 @@ import GoogleSignIn
 
 class ProfileViewController: UIViewController {
 
-    @IBOutlet var userNameLabel: UILabel!
-    @IBOutlet var signInButton: GIDSignInButton!
-    @IBOutlet var logoutButton: UIButton!
     var session: SessionProtocol?
+    
+    @IBOutlet var contentView: UIView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let user = session?.user {
-            let userpresenter = UserPresenterFactory.userPresenter(for: .profileLabel(user: user))
-            userNameLabel?.text = userpresenter?.userName
-            toggleLogoutButton(user: user)
         }
         GIDSignIn.sharedInstance()?.presentingViewController = self
-        signInButton.colorScheme = .light
     }
     
     @IBAction func logoutButtonPressed(_ sender: UIButton) {
@@ -32,7 +28,6 @@ class ProfileViewController: UIViewController {
     }
     
     func toggleLogoutButton(user: User){
-        logoutButton?.isHidden = user is UserNotLogged
     }
     
 }
@@ -43,8 +38,6 @@ extension ProfileViewController: PropertyObserver {
     
     func willChange(propertyName: String, newPropertyValue: Any?) {
         if propertyName == Session.SessionKeys.userKey, let user = newPropertyValue as? User {
-            let userpresenter = UserPresenterFactory.userPresenter(for: .profileLabel(user: user))
-            userNameLabel?.text = userpresenter?.userName
             print("userNameLabel as changed to \(user.name)")
             toggleLogoutButton(user: user)
         }
